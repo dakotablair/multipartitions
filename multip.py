2e6ffef774821854a7dc1d594557b5f9e06d8057
Dakota Blair <dakota@tensen.net>
2016-12-14 14:13:44 +0100

    Added comments, shebang.

--- multip.py
+++ multip.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+"""
+Let the number of partitions of Gaussian integers a+bi into parts u+vi
+so that u>0 and v>0. Compute $p(a, b)$ for 1 <= a <= NN, 1 <= b <= NN.
+"""
 import sys
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from fractions import gcd
 
 def timed(func):
@@ -18,7 +23,8 @@ def timed(func):
 
 class memoize(dict):
     """
-    https://wiki.python.org/moin/PythonDecoratorLibrary#Alternate_memoize_as_dict_subclass
+    https://wiki.python.org/moin/PythonDecoratorLibrary
+    #Alternate_memoize_as_dict_subclass
     """
     def __init__(self, func):
         self.func = func
@@ -31,19 +37,23 @@ class memoize(dict):
         return result
 
 @memoize
-def gcd_(a, b):
+def gcd_(a, b): 
+    """Wrapper for gcd to easily swap out more efficient algorithms"""
     return gcd(a, b)
 
 def mult(a, b):
+    """Wrapper for multiplication"""
     return a*b
 
 @memoize
 def inner(k1, k2):
+    """Inner sum in recurrence"""
     D = gcd_(k1, k2)
     return sum([k1//t for t in range(1, gcd_(k1,k2)+1) if D//t == D/t])
 
 @memoize
 def p(a, b):
+    """Table to be computed"""
     if b > a:
         return p(b, a)
     if(a==0 and b==0):
@@ -53,7 +63,7 @@ def p(a, b):
     if(a==1 or b==1):
         return 1
     if(b==2):
-        return a//2 + 1
+        return a//2 + 1 # Exercise: prove this!
     return sum([
         mult(p(a-k1, b-k2), inner(k1, k2))
         for k1 in range(1,a+1)
@@ -63,8 +73,8 @@ def p(a, b):
 if __name__ == "__main__":
     NN = int(sys.argv[-1]) 
     """
-    If this script were any more complicated
-    then I would have used argparse.
+    If this script gets any more complicated
+    then argparse should be used.
     """
     data = timed(lambda N:[[p(x, y) for y in range(1,N)] for x in range(1,N)])(NN)
     print([data[i][i] for i in range(len(data[0]))])
